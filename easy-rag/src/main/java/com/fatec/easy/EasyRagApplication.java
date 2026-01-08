ef907093ebebfb7bc1a1459408bdc943a41b528e
almeida1 <engsoftware1@gmail.com>
2026-01-08 19:32:38 -0300

    doc - documentação ajustada na classe controller

--- easy-rag/src/main/java/com/fatec/easy/EasyRagApplication.java
+++ easy-rag/src/main/java/com/fatec/easy/EasyRagApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Classe principal da aplicação - recuperação semântica de documentos
+ * RAG Ingenuo - implementa o ciclo fundamental retrieve-read sem otimizações
+ * de pré-recuperação (na query) ou pos-recuperação (na resposta)
+ * É funcional para protótipos e casos de uso simples, mas pode sofrer com
+ * problemas comuns dessa abordagem, como baixa precisão em perguntas complexas
+ * ou "alucinações" se o contexto recuperado não for perfeitamente relevante.
  */
 @SpringBootApplication
 public class EasyRagApplication {
